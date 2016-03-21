7a585afa665b5a49165b0a1be5b6ff78db79cf9e
VladR <averdalv@gmail.com>
2016-03-21 17:03:25 +0200

    add project

---
+++ PokerBaseEntity/ViewModel/ViewModelBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerBaseEntity.ViewModel
+{
+    class ViewModelBase:INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+
+    }
+}
